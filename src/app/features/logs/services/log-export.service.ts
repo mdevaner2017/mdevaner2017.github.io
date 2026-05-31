41aaa8005660652e46ff991dcb89831ad2d9a0e6
LucasLevyOB <oliveiralucaslevy@gmail.com>
2026-05-31 17:49:54 -0300

    feat: ajustes no arquivo exportado

--- src/app/features/logs/services/log-export.service.ts
+++ src/app/features/logs/services/log-export.service.ts
@@ -9,14 +9,14 @@ import { formatarDataHoraPadrao, tempoTotalEmMinutosSegundos } from '../../../ut
 })
 export class LogExportService {
 
-  constructor(@Inject(DOCUMENT) private document: Document) {}
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   private formatarExecucoes(execucoes: Execucao[]): string {
     let txt = "";
     const separador = "\n===================================\n\n";
     execucoes.forEach((execucao, index) => {
       txt += separador;
-      txt += `Execução ${index + 1}: ${formatarDataHoraPadrao(execucao.timestamp)}\n\n`;
+      txt += `Compilação ${index + 1}: ${formatarDataHoraPadrao(execucao.timestamp)}\n\n`;
       txt += `Código:\n${execucao.codigo}\n\n`;
       txt += `Saída:\n${execucao.saida}\n\n`;
     });
@@ -36,11 +36,12 @@ export class LogExportService {
     const dataInicio = formatarDataHoraPadrao(log.dataHoraInicio);
     const dataFim = formatarDataHoraPadrao(log.dataHoraFim!);
     const tempoTotal = tempoTotalEmMinutosSegundos(log.dataHoraInicio, log.dataHoraFim ?? new Date());
-    
-    let txt = `Início: ${dataInicio}\n`;
-    txt += `Fim: ${dataFim}\n`;
+
+    let txt = `Data Início: ${dataInicio}\n`;
+    txt += `Data Fim: ${dataFim}\n`;
     txt += `Tempo total: ${tempoTotal}\n`;
-    txt += `Execuções:\n${this.formatarExecucoes(log.execucoes)}\n`;
+    txt += `Quantidade de Compilações: ${log.execucoes.length}\n`;
+    txt += `Compilações:\n${this.formatarExecucoes(log.execucoes)}\n`;
 
     const nomeArquivo = this.getNomeArquivo(log.dataHoraInicio, log.dataHoraFim);
     this.downloadTxtFile(nomeArquivo, txt);
